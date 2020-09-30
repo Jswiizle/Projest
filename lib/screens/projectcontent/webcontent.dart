61f19f0ecce54ff830480daae4293cd899897884
Jeffrey Small <jeffreyhsmall@gmail.com>
2020-09-29 23:21:30 -0400

    nice

---
+++ lib/screens/projectcontent/webcontent.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WebContent extends StatefulWidget {
+  @override
+  _WebContentState createState() => _WebContentState();
+}
+
+class _WebContentState extends State<WebContent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
